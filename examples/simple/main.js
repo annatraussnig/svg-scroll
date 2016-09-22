2681acfca6e075bb60e0dfad8b5b5704991a88f1
Anna Traussnig <anna.traussnig@gmail.com>
2016-09-22 11:11:48 +0200

    Put examples in a separate folder

--- examples/simple/main.js
+++ examples/simple/main.js
@@ -1,4 +1,4 @@
-// ELEMENTS
+// ELEMENT
 var wrappedElement = new svgScroll.ScrollWrapper('#drawing');
 
 // INITIAL STATE
@@ -7,11 +7,11 @@ document.addEventListener('DOMContentLoaded', function(){
     window.scrollTo(0,0);
 }, false);
 
-window.onbeforeunload = function(){ window.scrollTo(0,0); }
-
 // ANIMATION
 window.addEventListener("scroll", function(e) {
     wrappedElement.reveal([0, 0.8], [0, 1]);
     wrappedElement.changeOnScroll([0.7, 0.9], 'stroke', ['#bc3c2f', '#ecd093']);
     wrappedElement.changeOnScroll([0.9, 1], 'opacity', [1, 0]);
 });
+
+window.onbeforeunload = function(){ window.scrollTo(0,0); }
